5f1c792dd7aafab93fe8ad0f801cac8d1ab145e1
yagamiar15 <dario11elme@hotmail.com>
2022-07-12 18:54:17 -0300

    creados estilos, refactorizada la funcion principal

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const btnEncriptar = document.querySelector('#btnEncriptar');
+/* const btnEncriptar = document.querySelector('#btnEncriptar');
 
 const btnDesencriptar = document.querySelector('#btnDesencriptar');
 
@@ -68,3 +68,25 @@ function limpiarHTML() {
     }
 }
 
+ */
+
+function botonEncriptar(){
+    const encriptar = document.getElementById('textoAEncriptar').value;
+    const encriptado = encriptar.replace(/(e)/gi, 'enter').replace(/(i)/gi, 'imes').replace(/(a)/gi, 'ai').replace(/(o)/gi, 'ober').replace(/(u)/gi, 'ufat');
+    document.getElementById("textoEncriptado").value = encriptado
+    document.getElementById("textoAEncriptar").value=" ";
+    // cambiarMensajeResultado();
+}
+
+var button = document.getElementById("btnEncriptar");
+button.onclick = botonEncriptar;
+
+function botonDesencriptar(){
+    const desencriptar = document.getElementById('textoAEncriptar').value;
+    const desencriptado = desencriptar.replace(/(enter)/gi, 'e').replace(/(imes)/gi, 'i').replace(/(ai)/gi, 'a').replace(/(ober)/gi, 'o').replace(/(ufat)/gi, 'u');
+    document.getElementById("textoEncriptado").value = desencriptado;
+    document.getElementById("textoAEncriptar").value;
+}
+
+var button2 = document.getElementById("btnDesencriptar");
+button2.onclick = botonDesencriptar;
