9a58d87be35e8017d95c61f267cd4e86106baee1
yagamiar15 <dario11elme@hotmail.com>
2022-07-13 23:09:42 -0300

    eliminado codigo innecesario, refactorizadas funciones

--- scripts/main.js
+++ scripts/main.js
@@ -1,84 +1,12 @@
-/* const btnEncriptar = document.querySelector('#btnEncriptar');
-
-const btnDesencriptar = document.querySelector('#btnDesencriptar');
-
-let divResultado = document.querySelector('#resultado');
-
-let botonCopiar;
-
-btnEncriptar.addEventListener('click', encriptar);
-btnDesencriptar.addEventListener('click', desencriptar);
-
-
-function leerTexto () {
-    const texto = document.querySelector('#textoIngresado');
-    if (texto.value == 0) {
-        alert('Ingrese un texto');  
-    } else { 
-    return texto.value.toLowerCase(); 
-    }
-}
-
-function encriptar(e) {
-    e.preventDefault();
-    limpiarHTML();
-
-    let textoEncriptar = leerTexto();
-
-    let mostrarEncriptado = encriptacion(textoEncriptar);
-
-    document.querySelector('#textoDevuelto').value = mostrarEncriptado;
-    crearBotonCopiar();
-
-}
-
-function desencriptar(e) {
-    e.preventDefault();
-    limpiarHTML();
-
-    let textoDesencriptar = leerTexto();
-    
-    let mostrarDesencriptado = desencriptacion(textoDesencriptar);
-
-    document.querySelector('#textoDevuelto').value = mostrarDesencriptado;
-    crearBotonCopiar();
-}
-
-function crearBotonCopiar() {
-    botonCopiar = document.createElement('button');
-
-    botonCopiar.innerHTML = 'Copiar';
-
-    divResultado.appendChild(botonCopiar);
-    
-    botonCopiar.addEventListener('click', copiarTexto);
-}
-
-function copiarTexto(e) {
-    e.preventDefault();
-    const textoDevuelto = document.querySelector('#textoDevuelto');
-    textoDevuelto.select();
-    navigator.clipboard.writeText(textoDevuelto.value);
-}
-
-function limpiarHTML() {
-    while (divResultado.lastChild == botonCopiar) {
-
-        divResultado.removeChild(botonCopiar);
-    }
-}
-
- */
-
 function botonEncriptar(){
     const encriptar = document.getElementById('textoAEncriptar').value;
     const encriptado = encriptar.replace(/(e)/gi, 'enter').replace(/(i)/gi, 'imes').replace(/(a)/gi, 'ai').replace(/(o)/gi, 'ober').replace(/(u)/gi, 'ufat');
     document.getElementById("textoEncriptado").value = encriptado
     document.getElementById("textoAEncriptar").value=" ";
-    // cambiarMensajeResultado();
+    cambiarMensajeResultado();
 }
 
-var button = document.getElementById("btnEncriptar");
+let button = document.getElementById("btnEncriptar");
 button.onclick = botonEncriptar;
 
 function botonDesencriptar(){
@@ -88,5 +16,5 @@ function botonDesencriptar(){
     document.getElementById("textoAEncriptar").value;
 }
 
-var button2 = document.getElementById("btnDesencriptar");
+let button2 = document.getElementById("btnDesencriptar");
 button2.onclick = botonDesencriptar;
